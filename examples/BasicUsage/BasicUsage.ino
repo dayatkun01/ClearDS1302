055fb00a5bec0d6f1e2d4411cb57c560d4691683
dayatkun01 <rahmatcraft21544@gmail.com>
2025-09-24 19:55:09 +0900

    Update BasicUsage.ino
    The RTC call name did not valid, fixed thx for someone that make issue.

--- examples/BasicUsage/BasicUsage.ino
+++ examples/BasicUsage/BasicUsage.ino
@@ -11,12 +11,13 @@ void setup() {
   Serial.begin(9600);
 
   // Time Set: second, minute, hour, day (0=Sunday - 7 = Saturday), date, month, year
-  rtc.set.time(0, 30, 12, 2, 26, 7, 2025); // Example: Monday 26 July 2025, 12:30:00
+  RTC1.set.time(0, 30, 12, 2, 26, 7, 2025); // Example: Monday 26 July 2025, 12:30:00
 }
 
 void loop() {
   // Show full time with format
-  Serial.println(rtc.get.time.full());
+  Serial.println(RTC1.get.time.full());
 
   delay(1000); // refresh every second
 }
+
